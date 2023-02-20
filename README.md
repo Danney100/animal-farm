0524da7564705591ef1faa72a7106758f3768545
Danney100 <comethale772@gmail.com>
2023-02-20 21:38:36 +0530

    Fixed minor ui

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Go to project directory.
 
 Then, Start web server using :  node server.js 
+
+We used foreca open source apis to fetch weather data. -> 'https://fnw-us.foreca.com/api/v1'
+
+Also, We used axios and materialize cdns to data feating and components designing.
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var http = require("http");
 var fs = require("fs");
 
-const PORT = 5000;
+var path = require("path");
+
+const PORT = 5001;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
+  var filePath = "." + req.url;
+  if (filePath == "./") filePath = "./index.html";
+
+  var extname = path.extname(filePath);
+  var contentType = "text/html";
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+    case ".wav":
+      contentType = "audio/wav";
+      break;
+  }
+
+  res.writeHead(200, { "content-type": contentType });
   fs.createReadStream("index.html").pipe(res);
 });
 
 server.listen(PORT);
+
+console.log(`Server started on port ${PORT}`);
